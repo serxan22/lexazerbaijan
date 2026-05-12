99002038b975774cf42e37f61db835632cd5a792
Sarkhan <smahabbatli18137@ada.edu.az>
2026-05-12 19:15:37 +0400

    Add mobile favicon png icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,15 @@ export const metadata: Metadata = {
     type: "website"
   },
   icons: {
-    icon: "/icon.svg",
-    shortcut: "/icon.svg",
-    apple: "/apple-icon.svg"
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
+    ],
+    shortcut: "/favicon-32x32.png",
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+    ]
   },
   verification: {
     google: "xepOVrbFn0xgib-lMvAR61HFrmAH_weyvIGithHKzPY"
